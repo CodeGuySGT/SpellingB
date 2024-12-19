6947f3040449618726d9125bebe2eb074f9df712
Sherrick <sherrick.thuesmunn@gmail.com>
2024-12-18 21:48:20 -0800

    Abstract the file reading part of getPangram() into helper function

--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,9 @@ const { getDownloadURL, getStorage } = require('firebase-admin/storage');
 const app = admin.initializeApp();
 const storage = admin.storage(app);
 
+const pangramsPath = "filtered_words.txt";
+const wordsPath = "word.list";
+
 // GET request event handler; returns a random pangram
 exports.getPangram = onRequest((request, response) => {
   return cors(request, response, async () => {
@@ -18,22 +21,36 @@ exports.getPangram = onRequest((request, response) => {
   })
 });
 
+// Downloads the file at the specified path in Firebase storage
+// and returns all the text within it as a single string
+const getFile = async (path) => {
+  const file = getStorage().bucket().file(path);
+
+  try {
+    const downloadURL = await getDownloadURL(file);
+    const response = await fetch(downloadURL);
+    const text = await response.text();
+
+    return text;
+  } catch (error) {
+    console.error('Error reading file:', error);
+    return '';
+  }
+}
+
 // Helper method to read from pangram list file in Firebase storage and pick a word
 // at random, then return that word
 const choosePangram = async () => {
-  const pangramList = getStorage().bucket().file("filtered_words.txt");
+  const text = await getFile(pangramsPath); // should be a long string full of pangrams
   const pangramLine = randPangramLine(); // choose random line index for pangram word
 
   try {
-    const downloadURL = await getDownloadURL(pangramList);
-    const response = await fetch(downloadURL);
-    const text = await response.text();
-    const lines = text.split('\n');
-    const pangram = lines[pangramLine];
+    const lines = text.split('\n'); // split single string text into an array of pangram strings
+    const pangram = lines[pangramLine]; // Grab the pangram at the specified random array index
 
     return pangram;
   } catch (error) {
-    console.error('Error reading file:', error);
+    console.error('Error parsing file:', error);
     return '';
   }
 };
