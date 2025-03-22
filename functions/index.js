c3c1b4e4728be19db9922f1f2b70eaed79fbf586
Sherrick <sherrick.thuesmunn@gmail.com>
2025-03-21 17:32:17 -0700

    Move helper functions to helpers.js file; change storage rules; stub out helper functions in getPangram unit test

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,8 @@
 const admin = require('firebase-admin');
 const { onCall } = require("firebase-functions/v2/https");
-const { getDownloadURL, getStorage } = require('firebase-admin/storage');
+const helpers = require('./helpers.js');
 
 admin.initializeApp();
-
-const pangramsPath = "filtered_words.txt";
-const wordsPath = "word.list";
 
 // GET request event handler; returns a random pangram
 exports.getPangram = onCall(
@@ -15,7 +12,7 @@ exports.getPangram = onCall(
 
     let pangram;
     try {
-      pangram = await choosePangram();
+      pangram = await helpers.choosePangram();
     } catch (e) {
       console.error(e);
     }
@@ -29,7 +26,7 @@ exports.getPangram = onCall(
 
     // Build a string 7 chars long from the unique letters in the set
     let letterString = "";
-    const shuffled = shuffleSet(uniqueLetters);
+    const shuffled = helpers.shuffleSet(uniqueLetters);
     shuffled.forEach((letter) => {
       letterString += letter;
     })
@@ -38,7 +35,7 @@ exports.getPangram = onCall(
     // Grab a letter at random to be anchor
     let randIndex; 
     try {
-      randIndex = randomInteger(0, letterString.length - 1);
+      randIndex = helpers.randomInteger(0, letterString.length - 1);
     } catch (e) {
       console.error(e);
     }
@@ -46,7 +43,7 @@ exports.getPangram = onCall(
     
     let wordsArray;
     try {
-      wordsArray = await validWords(letterString, anchorLetter);
+      wordsArray = await helpers.validWords(letterString, anchorLetter);
     } catch (e) {
       console.error(e);
     }
@@ -60,101 +57,3 @@ exports.getPangram = onCall(
     };
   }
 );
-
-// Takes a set as argument and returns an array with the elements shuffled
-function shuffleSet(set) {
-  const array = Array.from(set);
-
-  // Shuffle the array (using Fisher-Yates algorithm)
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-
-  return new Set(array);
-}
-
-// Downloads the file at the specified path in Firebase storage
-// and returns all the text within it as a single string
-const dlFile = async (path) => {
-  const file = getStorage().bucket().file(path);
-
-  try {
-    const downloadURL = await getDownloadURL(file);
-    const response = await fetch(downloadURL);
-    const text = await response.text();
-
-    return text;
-  } catch (error) {
-    console.error('Error reading file:', error);
-    return '';
-  }
-}
-
-// Reads from full word list text and return a list of words
-// matching a regex pattern
-// letterString = 7-letter string containing one copy of each unique letter from pangram
-// anchorLetter = unique letter chosen at random from letterString. All 'valid' words contain
-// the anchorLetter
-const validWords = async (letterString, anchorLetter) => {
-
-  if (letterString == null || anchorLetter == null) {
-    throw new Error('Null arguments');
-  }
-
-  // Save dictionary text as one large string
-  const text = await dlFile(wordsPath);
-
-  // Define a regex pattern and use it to find all matches from the input text
-  const pattern = new RegExp(`^[${letterString}]*${anchorLetter}[${letterString}]*$`, 'gm'); 
-  
-  // array containing all valid words
-  const matches = text.match(pattern); 
-
-  // Array containing only words between length 4 and 20 from above array
-  const correctLengthMatches = []; 
-  for (const match of matches) {
-    if (match.length >= 4 && match.length <= 20) {
-      correctLengthMatches.push(match);
-    }
-  }
-
-  return correctLengthMatches;
-}
-
-// Helper method to read from pangram list text from Firebase storage and pick a word
-// at random, then return that word
-const choosePangram = async () => {
-  const text = await dlFile(pangramsPath); // should be a long string full of pangrams
-  const pangramLine = randPangramLine(); // choose random line index for pangram word
-
-  try {
-    const lines = text.split('\r\n'); // split single string text into an array of pangram strings
-    const pangram = lines[pangramLine]; // Grab the pangram at the specified random array index
-
-    return pangram;
-  } catch (error) {
-    throw new Error('Error parsing file:', error);
-  }
-};
-
-// Returns a random number in range min-max, both inclusive
-function randomInteger(min, max) {
-  if (min > max) {
-    throw new Error('Invalid range');
-  }
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-// Uses randomInteger RNG to choose a random line number from the lines in pangram list (filtered_words.txt)
-function randPangramLine() {
-  // Count of lines in Pangram list, I don't anticipate this changing unless I switch word lists or update pangram definition
-  const linesCount = 54825; 
-  let index = null;
-  try {
-    index = randomInteger(0, linesCount);
-  } catch (e) {
-    console.error(e);
-  }
-  return index;
-}
--- functions/test/index.test.js
+++ functions/test/index.test.js
@@ -1,12 +1,80 @@
-const test = require('firebase-functions-test')({
-    reCAPTCHAKey: process.env.REACT_APP_RECAPTCHA_SITE_KEY,
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID
-  }, process.env.REACT_APP_SVC_ACCT_KEY_PATH);
-  const myFunctions = require('functions/index.js');
+const fs = require('node:fs');
+const admin = require('firebase-admin');
+const { assert } = require('chai');
+const { before, after, describe, it } = require('mocha');
+const sinon = require('sinon');
+const test = require('firebase-functions-test')();
+
+describe('Cloud Functions', () => {
+
+  // Declare debug token
+  let debugToken;
+
+  // Declare admin stub
+  let adminInitStub;
+
+  // Declare imports
+  let myFunctions;
+  let myHelpers;
+
+  // Declare helper function stubs
+  let choosePangramStub;
+  let shuffleSetStub; 
+  let randomIntegerStub;
+  let validWordsStub;
+
+  before(() => {
+
+    // Read and store debug token at runtime
+    const tokenPath = './secrets/debug_token.txt';
+    try {
+        debugToken = fs.readFileSync(tokenPath, 'utf-8').trim();
+    } catch (error) {
+        console.error('Error reading debug token:', error);
+        throw error;
+    }
+
+    // Stub admin.initializeApp to be a dummy function that doesn't do anything.
+    adminInitStub = sinon.stub(admin, 'initializeApp');
+
+    // Require index.js and helper functions and save the exports inside a namespace called myFunctions.
+    myFunctions = require('../index.js');
+    myHelpers = require('../helpers.js');
+
+    // Initialize stubs for each helper function
+    choosePangramStub = sinon.stub(myHelpers, 'choosePangram');
+    shuffleSetStub = sinon.stub(myHelpers, 'shuffleSet'); // not necessary?
+    randomIntegerStub = sinon.stub(myHelpers, 'randomInteger'); // not necessary?
+    validWordsStub = sinon.stub(myHelpers, 'validWords');
+
+    choosePangramStub.returns('subscribe');
+    shuffleSetStub.returns(new Set(['s','u','b','c','r','i','e']));
+    randomIntegerStub.returns(4);
+    validWordsStub.returns(['beurre', 'bibberies', 'burrs', 'crib', 'suburbs', 'succubi', 'scrubbers']);
+  });
+
+  after(() => {
+    // Restore stubs to original methods.
+    adminInitStub.restore();
+    choosePangramStub.restore();
+    shuffleSetStub.restore();
+    randomIntegerStub.restore();
+    validWordsStub.restore();
+    
+    test.cleanup();
+  });
+
+  describe('getPangram', function () {
+    it('Returns valid pangram-related JSON', async function () {
+        
+      // Wrap function, call it, and save results to 'result' object
+      const wrapped = test.wrap(myFunctions.getPangram);
+      const result = await wrapped();
+
+      // Test for null response -- WIP
+      assert(result !== null, 'Null response object');
+      assert(result.pangram !== null, 'Null pangram')
+    });
+  });
+  
+})
